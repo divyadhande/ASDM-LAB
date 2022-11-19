61e7e74bed7f8b554916b1184173e9518493dd59
adarshsoni25 <adarshsoni25@gmail.com>
2022-11-19 12:12:37 +0530

    done

---
+++ Product.java
@@ -0,0 +1,14 @@
+class Product {
+
+  public static void main(String[] args) {
+    
+    System.out.println("product of two numbers");
+    int x = 10;
+    int y = 20;
+    
+    System.out.println(x + " and " + y);
+
+    int pro = x * y;
+    System.out.println("The product is: " + pro);
+  }
+}
